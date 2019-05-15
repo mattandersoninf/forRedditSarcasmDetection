8ed0e5b1b449211ceda4756b3c4ad7cae2d59bdf
mattandersoninf <matt.anderson.inf@gmail.com>
2019-05-15 02:30:22 -0400

    1. redditAPI/main.py: You can now view comment body text, the author, the time that it was posted in, and the subreddit it derives from 2. data/about-data.txt: text file to explain what is in parameter of future csv file that will contina the scraped data

--- redditAPI/main.py
+++ redditAPI/main.py
@@ -1,6 +1,7 @@
 import config
-
 import praw
+import os
+import csv
 
 # make reddit object to access the API and enter the credentials as per the config file
 
@@ -10,16 +11,59 @@ reddit = praw.Reddit(client_id = config.client_id,
                      password = config.password,
                      user_agent = config.user_agent)
 
+"""
 # an example dataset has compiled comments from the following subreddits
 # AskReddit, politics, worldnews, leagueoflegends, pcmasterrace, news, funny, pics, todayileanred, GlobalOffensive, AlexJones, The_Donald, 
 # JoeRogan, vegan, Overwatch, apexlegends, CallOfDuty, ukpolitics, oddlysatisfying, conspiracy, AskAnAmerican, DotA2, wow, 
 # firstworldanarchists, youtubehaiku, Whatcouldgowrong, LivestreamFail, FIFA, cringe, uncensorednews, worldpolitics, JusticeServed,
 # showerthoughts, leagueoflegends, BlackPeopleTwitter, space, DestinyTheGame, TopMindsOfReddit, FloridaMan, ImGoingToHellForThis,
 # anime_irl, cringe, BigBrother
+"""
 
-subreddit = reddit.subreddit('funny')
+subreddits = ['AskReddit']
+#,'politics','BlackPeopleTwitter','space','TopMindsOfReddit','vegan','Overwatch','wow','The_Donald']
 
-hot_python = subreddit.hot(limit=1)
+subreddit = reddit.subreddit('AskReddit')
+
+# get the 10 most popular reddit comments in the defined subreddit 
+hot_python = subreddit.hot(limit=3)
+
+# for now, the goal of the script is to be able to extract the parameters mentioned in the 
+
+
+# comment.body is for getting the text from each comment, it seems that it the api digs into the replies 10 levels deep
+# do research into the controversiality parameter of comments
+
+# convert utc parameter possibly
 
 for submission in hot_python:
-    print(submission)
+    comments = submission.comments.list()
+    i = str(0)
+    for comment in comments:
+        try:
+            print('\n'+i+'. Comment Body: '+str(comment.body))
+            print('\n'+i+'. Comment Author: '+str(comment.author))
+            print('\n'+i+'. Comment Post Time: '+str(comment.created_utc))
+            print('\n'+i+'. Comment Subreddit: '+str(comment.subreddit))
+            i = str(int(i)+1)
+            if int(i) > 10:
+                break
+        except praw.exceptions.PRAWExceotion:
+            pass
+
+"""
+# this with statement opens up the untrained-data.csv file in the data folder
+# whatever
+with open('../data/untrained-data.csv', mode ='a') as udcsv:
+    if 
+    for sr in subreddits:
+        subreddit = reddit.subreddit(sr)
+
+        # get the 10 most popular reddit comments in the defined subreddit 
+        hot_python = subreddit.hot(limit=10)
+
+        # for now, the goal of the script is to be able to extract the parameters mentioned in the 
+
+        for submission in hot_python:
+            udcsv.write
+"""
