f6a58fb987486f1daf004a41d63cc87862cc7416
mattandersoninf <matt.anderson.inf@gmail.com>
2019-05-16 08:05:20 -0400

    1. redditAPI/main.py tried to add in the other subreddits but it is throwing back an error code 504, will re run for a dataset attempt

--- redditAPI/main.py
+++ redditAPI/main.py
@@ -2,63 +2,102 @@ import config
 import praw
 import os
 import csv
+import pandas as pd
+from pprint import pprint
+import multiprocessing
 
 # make reddit object to access the API and enter the credentials as per the config file
 
-reddit = praw.Reddit(client_id = config.client_id,
-                     client_secret = config.client_secret,
-                     username = config.username,
-                     password = config.password,
-                     user_agent = config.user_agent)
+#def getSubredditDataMP(sr):
+
+
 
 """
 # an example dataset has compiled comments from the following subreddits
-# AskReddit, politics, worldnews, leagueoflegends, pcmasterrace, news, funny, pics, todayileanred, GlobalOffensive, AlexJones, The_Donald, 
-# JoeRogan, vegan, Overwatch, apexlegends, CallOfDuty, ukpolitics, oddlysatisfying, conspiracy, AskAnAmerican, DotA2, wow, 
-# firstworldanarchists, youtubehaiku, Whatcouldgowrong, LivestreamFail, FIFA, cringe, uncensorednews, worldpolitics, JusticeServed,
-# showerthoughts, leagueoflegends, BlackPeopleTwitter, space, DestinyTheGame, TopMindsOfReddit, FloridaMan, ImGoingToHellForThis,
-# anime_irl, cringe, BigBrother
+# AskReddit, politics, worldnews, leagueoflegends, pcmasterrace, news, funny, pics, todayileanred, GlobalOffensive, AlexJones, The_Donald, JoeRogan, vegan, Overwatch, apexlegends, CallOfDuty, ukpolitics, oddlysatisfying, conspiracy, AskAnAmerican, DotA2, wow, firstworldanarchists, youtubehaiku, Whatcouldgowrong, LivestreamFail, FIFA, cringe, uncensorednews, worldpolitics, JusticeServed, showerthoughts, leagueoflegends, BlackPeopleTwitter, space, DestinyTheGame, TopMindsOfReddit, FloridaMan, ImGoingToHellForThis, anime_irl, cringe, BigBrother
+
+# remember to add a condition that verifies if the subreddit still exists
+
+# [pics, uncensorednews] don't work, expel for now
+
+# pics for example throws the following exception "prawcore.exceptions.NotFound: received 404 HTTP response", will review later
+
+# response 404 means that the resource does not exist. look further into what could have been removed from reddit
+
+# response 504 reddit is under heavy pressure, or you're trying to throw up a cooment into reddit's database (which this script does not do at all)
+
 """
 
-subreddits = ['AskReddit']
-#,'politics','BlackPeopleTwitter','space','TopMindsOfReddit','vegan','Overwatch','wow','The_Donald']
 
-subreddit = reddit.subreddit('AskReddit')
+subreddits = ['AskReddit', 'politics', 'worldnews', 'leagueoflegends', 
+              'pcmasterrace', 'news', 'funny', 'todayileanred', 
+              'GlobalOffensive', 'AlexJones', 'The_Donald', 'JoeRogan', 
+              'vegan', 'Overwatch', 'apexlegends', 'CallOfDuty', 
+              'ukpolitics', 'oddlysatisfying', 'conspiracy', 
+              'AskAnAmerican', 'DotA2', 'wow', 'firstworldanarchists',
+              'youtubehaiku', 'Whatcouldgowrong', 'LivestreamFail', 
+              'FIFA', 'cringe', 
+              'worldpolitics', 
+              'JusticeServed', 'showerthoughts', 'leagueoflegends', 
+              'BlackPeopleTwitter', 'space', 'DestinyTheGame', 
+              'TopMindsOfReddit', 'FloridaMan', 'ImGoingToHellForThis', 
+              'anime_irl', 'cringe', 'BigBrother']
 
-# get the 10 most popular reddit comments in the defined subreddit 
-hot_python = subreddit.hot(limit=3)
 
-# for now, the goal of the script is to be able to extract the parameters mentioned in the 
+def getSubredditData():
+    reddit = praw.Reddit(client_id = config.client_id,
+                        client_secret = config.client_secret,
+                        username = config.username,
+                        password = config.password,
+                        user_agent = config.user_agent)
 
 
-# comment.body is for getting the text from each comment, it seems that it the api digs into the replies 10 levels deep
-# do research into the controversiality parameter of comments
+    tmp_dataframe = pd.DataFrame(columns=['body','author','created_utc','subreddit','id','link_id','parent_id'])
+    
 
-# convert utc parameter possibly
+    #,'politics','BlackPeopleTwitter','space','TopMindsOfReddit','vegan','Overwatch','wow','The_Donald']
+    for sr in subreddits:
+        print(sr)
+        subreddit = reddit.subreddit(sr)
 
-for submission in hot_python:
-    comments = submission.comments.list()
-    i = str(0)
-    for comment in comments:
-        try:
-            print('\n'+i+'. Comment Body: '+str(comment.body))
-            print('\n'+i+'. Comment Author: '+str(comment.author))
-            print('\n'+i+'. Comment Post Time: '+str(comment.created_utc))
-            print('\n'+i+'. Comment Subreddit: '+str(comment.subreddit))
-            print('\n'+i+'. Comment ID: '+str(comment.id))
-            print('\n'+i+'. Comment Link_ID: '+str(comment.link_id))
-            print('\n'+i+'. Comment Parent_ID: '+str(comment.parent_id))
-            i = str(int(i)+1)
-            if int(i) > 10:
-                break
-        except praw.exceptions.PRAWExceotion:
-            pass
+        # get the 10 most popular reddit comments in the defined subreddit 
+        hot_python = subreddit.hot(limit=3)
+
+        # for now, the goal of the script is to be able to extract the parameters mentioned in the 
+
+
+        # comment.body is for getting the text from each comment, it seems that it the api digs into the replies 10 levels deep
+        # do research into the controversiality parameter of comments
+
+        # convert utc parameter possibly
+        
+        for submission in hot_python:
+            comments = submission.comments.list()
+            i = str(0)
+            for comment in comments:
+                try:
+                    tmp_dataframe = tmp_dataframe.append(pd.DataFrame({'body':[str(comment.body)], 
+                                                                       'author':[str(comment.author)], 
+                                                                       'created_utc':[str(comment.created_utc)], 
+                                                                       'subredit':[str(comment.subreddit)], 
+                                                                       'id':[str(comment.id)], 
+                                                                       'link_id':[str(comment.link_id)], 
+                                                                       'parent_id':[str(comment.parent_id)]}),ignore_index=True,sort=True)
+                    #tmp_dataframe.append(pd.DataFrame([comment.body, comment.author, comment.created_utc, comment.subreddit, comment.id, comment.link_id, comment.parent_id], columns=['body','author','created_utc','subreddit','id','link_id','parent_id']))
+                                  
+                    i = str(int(i)+1)
+                    if int(i) > 10:
+                        break
+                except praw.exceptions.PRAWException:
+                    pass
+
+    return tmp_dataframe
 
 """
 # this with statement opens up the untrained-data.csv file in the data folder
-# whatever
-with open('../data/untrained-data.csv', mode ='a') as udcsv:
-    if 
+
+with open('../data/untrained-data.csv', mode ='w') as udcsv:
+    
     for sr in subreddits:
         subreddit = reddit.subreddit(sr)
 
@@ -70,3 +109,13 @@ with open('../data/untrained-data.csv', mode ='a') as udcsv:
         for submission in hot_python:
             udcsv.write
 """
+
+
+if __name__=="__main__":
+
+    pprint(getSubredditData())
+
+    """
+    with multiprocessing.Pool() as pool:
+        pool.map(getSubredditDataMP, subreddits)
+    """
