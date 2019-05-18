37f4cebe1deea3d2baaebbfd50eb943b50e23839
mattandersoninf <matt.anderson.inf@gmail.com>
2019-05-17 23:32:07 -0400

    Made the first stable dataset
    main.py runs to make the csv file that creates an untrained-data.csv file to be used for tensorflow to create a model from. moved the data subfolder for convenience

---
+++ redditAPI/subreddit_file.py
@@ -0,0 +1,19 @@
+"""
+This file contains the subreddit that will be scraped to create datasets
+"""
+
+
+
+subreddits = ['AskReddit', 'politics', 'worldnews', 'leagueoflegends', 
+              'pcmasterrace', 'news', 'funny', 'todayileanred', 
+              'GlobalOffensive', 'AlexJones', 'The_Donald', 'JoeRogan', 
+              'vegan', 'Overwatch', 'apexlegends', 'CallOfDuty', 
+              'ukpolitics', 'oddlysatisfying', 'conspiracy', 
+              'AskAnAmerican', 'DotA2', 'wow', 'firstworldanarchists',
+              'youtubehaiku', 'Whatcouldgowrong', 'LivestreamFail', 
+              'FIFA', 'cringe', 
+              'worldpolitics', 
+              'JusticeServed', 'showerthoughts', 'leagueoflegends', 
+              'BlackPeopleTwitter', 'space', 'DestinyTheGame', 
+              'TopMindsOfReddit', 'FloridaMan', 'ImGoingToHellForThis', 
+              'anime_irl', 'cringe', 'BigBrother']
